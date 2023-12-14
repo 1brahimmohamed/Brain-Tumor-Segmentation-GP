5e33a26b264ebee8b2d7eda1de3dfe5671ad1452
Ibrahim <imimazim7@gmail.com>
2023-12-14 22:22:09 +0200

    feat: proxy logging changes

--- Software/backend/Gateway/app.js
+++ Software/backend/Gateway/app.js
@@ -9,60 +9,51 @@ const PORT = 4000;
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.json([
-    {
-      id: "1",
-      name: "Ibrahim Mohamed: Software",
-    },
-    {
-      id: "2",
-      name: "Mohamed Ismail: Software",
-    },
-    {
-      id: "3",
-      name: "Mahmoud Yaser: Artificial Intelligence",
-    },
-    {
-      id: "4",
-      name: "Ahmed Hassan: Artificial Intelligence",
-    },
-    {
-      id: "5",
-      name: "Maha Medhat: Artificial Intelligence",
-    },
-  ]);
+    res.json([
+        {
+            id: "1",
+            name: "Ibrahim Mohamed: Software",
+        },
+        {
+            id: "2",
+            name: "Mohamed Ismail: Software",
+        },
+        {
+            id: "3",
+            name: "Mahmoud Yaser: Artificial Intelligence",
+        },
+        {
+            id: "4",
+            name: "Ahmed Hassan: Artificial Intelligence",
+        },
+        {
+            id: "5",
+            name: "Maha Medhat: Artificial Intelligence",
+        },
+    ]);
 });
-
 
 // Define a route for the proxy server
 app.get('/proxy', (req, res) => {
-  const targetUrl = req.query.target || '';
-  console.log('Proxy request for:', targetUrl)
-  console.log('Request headers:', req.headers)
+    const targetUrl = req.query.target || '';
+    console.log('Proxy request for:', targetUrl)
+    console.log('Request headers:', req.headers)
 
-  // Determine the protocol (http or https) based on the target URL
-  const targetProtocol = targetUrl.startsWith('https://') ? https : http;
+    // Determine the protocol (http or https) based on the target URL
+    const targetProtocol = targetUrl.startsWith('https://') ? https : http;
 
-  targetProtocol.get(targetUrl, {
-    headers: req.headers
-  },(targetRes) => {
-    res.writeHead(targetRes.statusCode, targetRes.headers);
-    targetRes.pipe(res);
-}).on('error', (e) => {
-    console.log(e);
-});
-});
-
-
-app.get("/api", (req, res) => {
-  res.json({
-    id: "1",
-    name: "Hello Network",
-  })
+    targetProtocol.get(targetUrl, {
+        headers: req.headers
+    }, (targetRes) => {
+        res.writeHead(targetRes.statusCode, targetRes.headers);
+        targetRes.pipe(res);
+    }).on('error', (e) => {
+        console.log(e);
+    });
 });
 
 app.listen(PORT, () => {
-  console.log(
-    `Graduation Project node app on container is listening for requests on port ${PORT}`
-  );
+    console.log(
+        `Graduation Project node app on container is listening for requests on port ${PORT}`
+    );
 });
