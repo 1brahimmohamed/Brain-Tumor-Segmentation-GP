b203499bb0e0ecb0b6c7072dbe8814d52050996c
Ibrahim <imimazim7@gmail.com>
2024-02-15 05:29:05 +0200

    refactor: some code changes

---
+++ Software/frontend/src/features/Testing.tsx
@@ -0,0 +1,11 @@
+import {AboutModal} from "@ohif/ui";
+
+const Testing = () => {
+    return (
+        <div className="bg-primary-dark">
+            <AboutModal />
+        </div>
+    );
+};
+
+export default Testing;
