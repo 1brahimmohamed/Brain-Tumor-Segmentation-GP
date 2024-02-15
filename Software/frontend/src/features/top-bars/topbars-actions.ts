7d0e2d3db6945d66b0aab2941f7c1fff21064ff7
Ibrahim <imimazim7@gmail.com>
2024-02-15 05:42:21 +0200

    chore: added topbar actions

---
+++ Software/frontend/src/features/top-bars/topbars-actions.ts
@@ -0,0 +1,36 @@
+import store from '@/redux/store.ts';
+import { uiSliceActions } from '@ui/ui-slice.ts';
+
+const handleColorModeChange = () => {
+    store.dispatch(uiSliceActions.toggleTheme());
+};
+
+const handleLanguageChange = (lang: string) => {
+    store.dispatch(uiSliceActions.setCurrentLanguage(lang));
+};
+
+const handleSettingsItemClick = (setting: string) => {
+    switch (setting) {
+        case 'About':
+            // TODO: Add about modal.
+            break;
+        case 'License Agreement':
+            // TODO: Add license agreement modal.
+            break;
+        case 'Help':
+            // TODO: redirect to help page.
+            break;
+        case 'Logout':
+            // TODO: Add logout functionality.
+            break;
+        default:
+            break;
+    }
+};
+
+
+export {
+    handleColorModeChange,
+    handleLanguageChange,
+    handleSettingsItemClick
+};
