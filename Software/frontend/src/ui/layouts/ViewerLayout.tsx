fa9b0b7a2fdf2655994ecd0ba5bbced709e8fe8f
Bodykudo <bodyalsadek2002@gmail.com>
2024-06-17 22:48:41 +0300

    feat: implement delete series context menu on viewer sidebar

--- Software/frontend/src/ui/layouts/ViewerLayout.tsx
+++ Software/frontend/src/ui/layouts/ViewerLayout.tsx
@@ -5,13 +5,13 @@ import ViewerSidebar from '@features/viewer/StudySidebar/ViewerSidebar.tsx';
 import ViewerToolPanel from '@features/viewer/ViewerToolPanel/ViewerToolPanel.tsx';
 
 const ViewerLayout = () => {
-    window.addEventListener(
-        'contextmenu',
-        function (event) {
-            event.preventDefault(); // Prevent the default context menu from appearing
-        },
-        false
-    );
+    // window.addEventListener(
+    //     'contextmenu',
+    //     function (event) {
+    //         event.preventDefault(); // Prevent the default context menu from appearing
+    //     },
+    //     false
+    // );
 
     return (
         <div className={'w-full'}>
@@ -28,10 +28,10 @@ const ViewerLayout = () => {
                 <div className={'h-[93vh] w-2/12 max-w-52'}>
                     <ViewerSidebar className={'h-full'} />
                 </div>
-                <div className={'h-auto flex-grow w-11/12'}>
+                <div className={'h-auto flex-grow w-11/12'} onContextMenu={(e) => e.preventDefault()}>
                     <Outlet />
                 </div>
-                <div>
+                <div onContextMenu={(e) => e.preventDefault()}>
                     <ViewerToolPanel />
                 </div>
             </div>
