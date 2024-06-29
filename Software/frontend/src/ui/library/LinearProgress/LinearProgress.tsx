b90eb6caf86f26e9dd0611ca4abc0411b00eaf64
MohamedAIsmail <mohamedaismail214@gmail.com>
2024-06-29 12:07:42 +0300

    bug fix: fixing create_theme issue

--- Software/frontend/src/ui/library/LinearProgress/LinearProgress.tsx
+++ Software/frontend/src/ui/library/LinearProgress/LinearProgress.tsx
@@ -1,6 +1,6 @@
 import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { Box } from '@mui/material';
 
 const LinearProgressWithLabel = (props: LinearProgressProps & { value: number }) => {
     return (
